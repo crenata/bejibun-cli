ed1ccdbdf7ee6d2b4b7f0d10621c10a2018964c4
ghulje <ghulam@jejeharapan.com>
2025-12-01 11:01:48 +0700

    feat(cleansing): remove CHANGELOG.md, LICENSE, ROADMAP.md, & UI improvement

--- bin/configure.ts
+++ bin/configure.ts
@@ -23,8 +23,9 @@ if (isEmpty(directory)) {
     process.exit(1);
 }
 
-Logger.setContext(context).info("Cloning the project...");
-Logger.empty();
+Logger.separator();
+Logger.setContext(context).info("Cloning the Project");
+Logger.separator();
 
 Bun.spawnSync(["git", "clone", "https://github.com/crenata/bejibun.git", directory], {
     stdin: "inherit",
@@ -33,8 +34,9 @@ Bun.spawnSync(["git", "clone", "https://github.com/crenata/bejibun.git", directo
 });
 
 Logger.empty();
-Logger.setContext(context).info("Installing dependencies...");
-Logger.empty();
+Logger.separator();
+Logger.setContext(context).info("Installing Dependencies");
+Logger.separator();
 
 const workingDirectory: string = path.resolve(process.cwd(), directory);
 
@@ -46,18 +48,25 @@ Bun.spawnSync(["bun", "install"], {
 });
 
 Logger.empty();
-Logger.setContext(context).info("Removing .git directory...");
-Logger.empty();
+Logger.separator();
+Logger.setContext(context).info("Cleansing");
+Logger.separator();
 
-rmSync(path.resolve(workingDirectory, ".git"), {
-    recursive: true,
-    force: true
-});
+for (const clean of [".git", "CHANGELOG.md", "LICENSE", "ROADMAP.md"]) {
+    Logger.setContext("Cleansing").debug(`Removing ${clean}`);
+    rmSync(path.resolve(workingDirectory, clean), {
+        recursive: true,
+        force: true
+    });
+}
 
 Logger.empty();
-Logger.setContext(context).info("Creating .env...");
-Logger.empty();
+Logger.separator();
+Logger.setContext(context).info("Setup Environment");
+Logger.separator();
 
+Logger.setContext("Environment").debug("Copying .env.example to .env");
+Logger.setContext("Environment").debug("Set app name");
 await Bun.write(
     path.resolve(workingDirectory, ".env"),
     (
